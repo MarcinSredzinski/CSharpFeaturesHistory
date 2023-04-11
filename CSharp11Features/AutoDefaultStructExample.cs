cceb3dc54e4c3bc9adab74977bbbff279507d9e8
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-04-11 21:12:16 +0200

    Auto default struct example added

---
+++ CSharp11Features/AutoDefaultStructExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp11Features;
+
+public struct Point
+{
+    public int X;
+    public int Y;
+    public Point(int x, int y)
+    {
+        X = x;
+        //Y = y; Normally (up to c# 11) you would need to assign the value to all fields of a struct, but now default value is assigned behind the scenes. 
+    }
+}
