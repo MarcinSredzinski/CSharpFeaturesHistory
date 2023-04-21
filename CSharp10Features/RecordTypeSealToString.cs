9afd7fb9f47d176be171f93e0add708bab9d0ff9
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-04-21 23:01:04 +0200

    Record type seal to string example

---
+++ CSharp10Features/RecordTypeSealToString.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp10Features
+{
+     internal record Vehicle(int Mass, int MaxSpeed)
+    {        
+        public sealed override string ToString()
+        {
+            return $"Mass of the vehicle is: {Mass} and max speed is {MaxSpeed}";
+        }
+    }
+
+    internal record Bike : Vehicle
+    {
+        protected Bike(int Mass) : base(Mass, 0)
+        {
+        }
+        //And now it cannot be overriden here. 
+        //public override string ToString()
+        //{
+        //    return $"Mass of the bike is: {Mass} and the speed depends on the person riding the bike";
+        //}
+    }
+}
