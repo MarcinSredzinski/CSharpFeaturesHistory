d27c1a522ed455847660f0c2e9487b14799dea5d
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-04-25 22:19:51 +0200

    Init setters example added

---
+++ CSharp9Features/InitSettersExample.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp9Features;
+
+internal class InitSettersExample
+{
+    void Execute()
+    {
+        var person = new Person();
+       // person.Age = 50; - results in error. You can only set init statements in constructor, on base or in initialization of the object.
+
+    }
+}
+internal class Person
+{
+    //init - once set, it's readonly
+    public int Age { get; init; }
+}
