2bdfb181c2e5513fef5dcd7a5248af8307a712de
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-05-09 23:34:30 +0200

    Records example added

---
+++ CSharp9Features/RecordsExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp9Features
+{
+    internal class RecordsExample
+    {
+    }
+
+    public record TestPerson(string FirstName, string LastName);
+}
