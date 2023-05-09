691dfb22cf5f3789380cc04bf607ac3d67c43561
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-05-09 23:41:27 +0200

    cleanup

--- CSharp9Features/RecordsExample.cs
+++ CSharp9Features/RecordsExample.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CSharp9Features;
 
-namespace CSharp9Features
+internal class RecordsExample
 {
-    internal class RecordsExample
-    {
-    }
-
-    public record TestPerson(string FirstName, string LastName);
 }
+
+//reference type
+//immutable
+public record TestPerson(string FirstName, string LastName);
